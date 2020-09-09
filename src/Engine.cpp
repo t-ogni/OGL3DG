ed4092c07f477c53397150cbaca90bc7a6229e1f
Moonlincoder <moonlin.coder@gmail.com>
2020-09-10 00:22:00 +0300

    create some classes files

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,11 +1,11 @@
 //______  _______ _______ _______ _______ _______
 //|     \ |______    |    |______ |_____| |  |  |
-//|_____/.______| .  |    |______ |     | |  |  |               
+//|_____/.______| .  |    |______ |     | |  |  |
 // Copyright (c) 2020 Dark Shield Team. All rights reserved.
 // Created by moonlin on 006 06.09.20 at 20:46.
 
 #include "Engine.h"
 
-Engine::Engine() {};
+Engine::Engine() {}
 
-Engine::~Engine() {};
+Engine::~Engine() {}
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include <GLFW/glfw3.h>
 
 int main(){
-    glfwInit();
+    if(!glfwInit()){
+        return 0x0001;
+    }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -11,8 +13,30 @@ int main(){
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
+    GLFWwindow *window = glfwCreateWindow(500, 400, "test", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    if(window == nullptr){
+        glfwTerminate();
+        return 0x0002;
+    }
 
-    std::cout << "init" << std::endl;
+    std::cout << "init window" << std::endl;
 
+    int gladInitRes = gladLoadGL();
+    if (!gladInitRes) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return 0x0003;
+    }
+
+    while(!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
