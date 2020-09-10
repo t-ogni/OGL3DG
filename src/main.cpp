0d3af6cbe6ebf6dfe54527bcea9a27441e822bba
Moonlincoder <moonlin.coder@gmail.com>
2020-09-11 01:38:08 +0300

    create some classes files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <GL/gl.h>
 
 int main(){
     if(!glfwInit()){
@@ -19,7 +20,6 @@ int main(){
         glfwTerminate();
         return 0x0002;
     }
-
     std::cout << "init window" << std::endl;
 
     int gladInitRes = gladLoadGL();
@@ -29,9 +29,45 @@ int main(){
         return 0x0003;
     }
 
+
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+    // Массив из 3 векторов которые будут представлять 3 вершины
+    static const GLfloat g_vertex_buffer_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f,  1.0f, 0.0f,
+    };
+    GLuint vertexbuffer;
+
+// Сначала генерируем OpenGL буфер и сохраняем указатель на него в vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+// Биндим буфер
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+
+// Предоставляем наши вершины в OpenGL
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+
     while(!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        //glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+                0,                  // Атрибут 0. Сакрального смысла в нуле нет, но число должно совпадать с числом в шейдере
+                3,                  // количество
+                GL_FLOAT,           // тип
+                GL_FALSE,           // нормализировано ли?
+                0,                  // шаг
+                (void*)nullptr      // смещение в буфере
+        );
+
+// Рисуем треугольник !
+        glDrawArrays(GL_TRIANGLES, 0, 3); //Начиная с вершины 0 и рисуем 3 штуки. Всего => 1 треугольник
+        glDisableVertexAttribArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
