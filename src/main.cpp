e3930d99562ce79463e57ac7748667ec19be5b22
Moonlincoder <moonlin.coder@gmail.com>
2020-09-13 13:41:50 +0300

    moving rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <GL/gl.h>
 #include <cmath>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 int main(){
     if(!glfwInit()){
         return 0x0001;
@@ -15,6 +18,8 @@ int main(){
 
     GLFWwindow *window = glfwCreateWindow(500, 400, "test", nullptr, nullptr);
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     if(window == nullptr){
         glfwTerminate();
         return 0x0002;
@@ -29,41 +34,23 @@ int main(){
     }
 
 
-    GLuint VertexArrayID;
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-
-    static const GLfloat g_vertex_buffer_data[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f,
-    };
-    GLuint vertexbuffer;
-
-// Сначала генерируем OpenGL буфер и сохраняем указатель на него в vertexbuffer
-    glGenBuffers(1, &vertexbuffer);
-// Биндим буфер
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-
-// Предоставляем наши вершины в OpenGL
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
     char *vertexShaderSource = "#version 330 core\n"
                                "layout (location = 0) in vec3 aPos;\n"
-                               "out vec4 vertexColor;\n"
+                               "layout (location = 1) in vec3 aColor;\n"
+                               "out vec3 vertexColor;\n"
                                "void main()\n"
                                "{\n"
-                               "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                               "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+                               "   gl_Position = vec4(aPos, 1.0);\n"
+                               "   vertexColor = aColor;\n"
                                "}\0";
 
     char *fragmentShaderSource = "#version 330 core\n"
                                  "out vec4 FragColor;\n"
-                                 "in vec4 vertexColor;\n"
+                                 "in vec3 vertexColor;\n"
                                  "void main()\n"
                                  "{\n"
-                                 "   FragColor = vertexColor;\n"
-                                 "}\n\0";
+                                 "    FragColor = vec4(vertexColor, 1.0);\n"
+                                 "}\0";
 
     GLint success;
     GLchar infoLog[512];
@@ -81,7 +68,7 @@ int main(){
 
     GLuint fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
     glCompileShader(fragmentShader);
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if(!success){
@@ -104,8 +91,15 @@ int main(){
         return 0x0006;
     }
 
-
     glUseProgram(shaderProgram);
+
+    float vertices[] = {
+            // координаты         // цвета
+            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // нижняя правая вершина
+            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // нижняя левая вершина
+            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // верхняя вершина
+    };
+
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -114,10 +108,14 @@ int main(){
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // Координатный атрибут
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)nullptr);
     glEnableVertexAttribArray(0);
+    // Цветовой атрибут
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // Обратите внимание, что данное действие разрешено, вызов glVertexAttribPointer() зарегистрировал VBO как привязанный вершинный буферный объект для вершинного атрибута, так что после этого мы можем спокойно выполнить отвязку
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -127,16 +125,23 @@ int main(){
     glBindVertexArray(0);
 
     while(!glfwWindowShouldClose(window)){
+        processInput(window);
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        double timeValue = glfwGetTime();
 
-        float timeValue = glfwGetTime();
-        float greenValue = sin(timeValue) / 2.0f + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        vertices[0] = (float)sin(timeValue-90);
+        vertices[1] = (float)cos(timeValue+90);
+        vertices[6] = (float)sin(timeValue);
+        vertices[7] = (float)cos(timeValue);
+        vertices[12] = (float)sin(timeValue+90);
+        vertices[13] = (float)cos(timeValue-90);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
 
         // Переходим к рендерингу треугольников
-        glBindBuffer(VBO);
+        glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
@@ -145,4 +150,17 @@ int main(){
 
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // Убеждаемся, что окно просмотра соответствует новым размерам окна
+    // Обратите внимание, что высота будет значительно больше, чем указано на Retina-дисплеях
+    glViewport(0, 0, width, height);
 }
