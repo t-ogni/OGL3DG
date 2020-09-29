a7fa37cbbddf717a4c01d2bd563f50ee68a34ec9
Moonlincoder <moonlin.coder@gmail.com>
2020-09-30 00:00:29 +0300

    create window class

---
+++ include/Window.h
@@ -0,0 +1,23 @@
+//______  _______ _______ _______ _______ _______
+//|     \ |______    |    |______ |_____| |  |  |
+//|_____/.______| .  |    |______ |     | |  |  |
+// Copyright (c) 2020 Dark Shield Team. All rights reserved.
+// Created by moonlin on 029 29.09.20 at 23:58.
+
+#ifndef OGL3DG_WINDOW_H
+#define OGL3DG_WINDOW_H
+
+
+class Window {
+private:
+
+protected:
+
+public:
+    explicit Window(char *title, int width, int height);
+
+    ~Window();
+};
+
+
+#endif //OGL3DG_WINDOW_H
---
+++ src/Window.cpp
@@ -0,0 +1,11 @@
+//______  _______ _______ _______ _______ _______
+//|     \ |______    |    |______ |_____| |  |  |
+//|_____/.______| .  |    |______ |     | |  |  |
+// Copyright (c) 2020 Dark Shield Team. All rights reserved.
+// Created by moonlin on 029 29.09.20 at 23:58.
+
+#include "Window.h"
+
+Window::Window() {}
+
+Window::~Window() {}
