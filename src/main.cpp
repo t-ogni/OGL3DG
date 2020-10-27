a9f8d24ff545232901f2c5dbad6b4928df7acd69
Moonlincoder <moonlin.coder@gmail.com>
2020-10-27 18:17:07 +0300

    game code-style refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include "Game.h"
 #include "Engine.h"
 
-class Striker : public Game
-{
+class Striker : public Game {
 private:
     Object cube, deer;
     Shader shader;
@@ -11,48 +10,46 @@ private:
 
 public:
     explicit Striker() : Game() {
-        Console::message("Striker game inited");
+        Console::message("Striker game class created");
     };
-    void Init() override
-    {
+
+    void Init() override {
         shader("shaders/vertex/basic.vert", "shaders/fragment/basic.frag");
         cube("common/cube.obj");
         deer("common/Deer.obj");
+        engine->camera->speed = 10.0f;
     }
 
-    void ProcessInput(float dt) override
-    {
-        if(keys[GLFW_KEY_W]) this-> engine-> camera->forward(dt);
-        if(keys[GLFW_KEY_S]) this-> engine-> camera->backward(dt);
-        if(keys[GLFW_KEY_A]) this-> engine-> camera->left(dt);
-        if(keys[GLFW_KEY_D]) this-> engine-> camera->right(dt);
-        if(keys[GLFW_KEY_SPACE]) this-> engine-> camera-> up(dt);
-        if(keys[GLFW_KEY_LEFT_SHIFT]) this-> engine-> camera-> down(dt);
+    void ProcessInput(float dt) override {
+        if (engine->input->isKeyboardPressed(GLFW_KEY_W))
+            this->engine->camera-> forward(dt);
 
-        if(mouseChanged) {
-            engine-> camera-> changeDirection(mouseChangedX, mouseChangedY, dt);
-            mouseChanged = false;
-        }
+        if (engine->input->isKeyboardPressed(GLFW_KEY_S))
+            this->engine->camera-> backward(dt);
+
+        if (engine->input->isKeyboardPressed(GLFW_KEY_A))
+            this->engine->camera-> left(dt);
+
+        if (engine->input->isKeyboardPressed(GLFW_KEY_D))
+            this->engine->camera-> right(dt);
+
+        if (engine->input->isKeyboardPressed(GLFW_KEY_LEFT_SHIFT))
+            this->engine->camera-> up(dt);
+
+        if (engine->input->isKeyboardPressed(GLFW_KEY_LEFT_CONTROL))
+            this->engine->camera-> down(dt);
+
     }
 
-    void mouseMoved(double x, double y) override
-    {
-        mouseChanged = true;
-        mouseChangedX = (mouseX - x);
-        mouseChangedY = (mouseY - y);
-    }
-
-    void Render() override
-    {
-        //cube.draw(shader, engine-> camera-> getMVP());
-        deer.draw(shader, engine-> camera-> getMVP());
+    void Render() override {
+        cube.draw(shader, engine->camera->getMVP());
+        deer.draw(shader, engine->camera->getMVP());
     }
 
 };
 
 
-auto main() -> int
-{
+auto main() -> int {
     Striker MainGame;
     Engine MainEng(&MainGame);
     return MainEng.run();
