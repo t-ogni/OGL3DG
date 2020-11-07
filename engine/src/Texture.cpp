badb89e208b24e6bfb95519d1b123c9b1c965ee4
Moonlincoder <moonlin.coder@gmail.com>
2020-11-07 23:48:47 +0300

    glm re-include

---
+++ engine/src/Texture.cpp
@@ -0,0 +1,11 @@
+//______  _______ _______ _______ _______ _______
+//|     \ |______    |    |______ |_____| |  |  |
+//|_____/.______| .  |    |______ |     | |  |  |
+// Copyright (c) 2020 Dark Shield Team. All rights reserved.
+// Created by moonlin on 007 07.11.20 at 22:59.
+
+#include "Texture.h"
+
+Texture::Texture() {}
+
+Texture::~Texture() {}
---
+++ engine/src/Texture.h
@@ -0,0 +1,19 @@
+//______  _______ _______ _______ _______ _______
+//|     \ |______    |    |______ |_____| |  |  |
+//|_____/.______| .  |    |______ |     | |  |  |
+// Copyright (c) 2020 Dark Shield Team. All rights reserved.
+// Created by moonlin on 007 07.11.20 at 22:59.
+
+#ifndef OGL3DG_TEXTURE_H
+#define OGL3DG_TEXTURE_H
+
+
+class Texture {
+public:
+    Texture();
+
+    ~Texture();
+};
+
+
+#endif //OGL3DG_TEXTURE_H
